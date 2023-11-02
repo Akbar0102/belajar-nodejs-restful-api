d37e078f36f94b2372261a60b2bdd8b33c44c788
Akbar Hidayatuloh <01akbarh@gmail.com>
2023-11-02 15:47:37 +0700

    fix: rename api router

--- src/application/web.js
+++ src/application/web.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { errorMiddleware } from "../middleware/error-middleware.js";
 import { publicRouter } from "../route/public-api.js";
-import { userRouter } from "../route/api.js";
+import { apiRouter } from "../route/api.js";
 
 export const web = express();
 web.use(express.json());
 web.use(publicRouter);
-web.use(userRouter);
+web.use(apiRouter);
 web.use(errorMiddleware);
